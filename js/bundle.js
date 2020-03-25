61cb174f5b546939e6799c55d3d8867b1c8fd07e
Nomand <iamnomand@gmail.com>
2020-03-25 10:28:42 +0800

    rebuilding site 2020年 3月25日 星期三 10时28分42秒 CST

--- js/bundle.js
+++ js/bundle.js
@@ -1,87 +1,87 @@
 /**
  * serviceWorker
  */
-(function() {
+;(function() {
   if (navigator.serviceWorker && !location.host.includes('localhost')) {
     navigator.serviceWorker
       .register(
         location.host.includes('localhost')
           ? 'http://localhost:1313/sw.js'
-          : 'https://hasaki.xyz/sw.js'
+          : 'https://hasaki.xyz/sw.js',
       )
       .catch(error => {
-        console.error(`ServiceWorker registration failed: ${error}`);
-      });
+        console.error(`ServiceWorker registration failed: ${error}`)
+      })
 
     navigator.serviceWorker.ready.then(registration => {
       console.info(
         '👏👏👏 %c Service Worker registered success. ',
-        'color:#1534fa'
-      );
-    });
+        'color:#1534fa',
+      )
+    })
   }
-})();
+})()
 
 window.addEventListener('DOMContentLoaded', _ => {
   /**
    * Rewrite head anchor links
    */
-  (function() {
+  ;(function() {
     const ids = [].slice.call(
-      document.querySelectorAll('#markdown [id]:not(main)')
-    );
+      document.querySelectorAll('#markdown [id]:not(main)'),
+    )
 
-    const hash = location.hash;
+    const hash = location.hash
 
     ids.forEach($node => {
-      const text = $node.textContent;
-      const $a = document.createElement('a');
-      $a.setAttribute('href', `#${$node.getAttribute('id')}`);
-      $a.textContent = text;
-      $node.innerHTML = '';
-      $node.appendChild($a);
-    });
+      const text = $node.textContent
+      const $a = document.createElement('a')
+      $a.setAttribute('href', `#${$node.getAttribute('id')}`)
+      $a.textContent = text
+      $node.innerHTML = ''
+      $node.appendChild($a)
+    })
 
     if (hash) {
       const id = ids.find(
-        $node => $node.getAttribute('id') === hash.substr(1, hash.length)
-      );
-      setTimeout(_ => id && id.nodeType === 1 && id.firstChild.click(), 500);
+        $node => $node.getAttribute('id') === hash.substr(1, hash.length),
+      )
+      setTimeout(_ => id && id.nodeType === 1 && id.firstChild.click(), 500)
     }
-  })();
-});
+  })()
+})
 
 /**
  * Copy inject my blog information
  */
-(function() {
+;(function() {
   window.addEventListener(
     'copy',
     function(ev) {
-      const text = ev.clipboardData || window.clipboardData;
+      const text = ev.clipboardData || window.clipboardData
       if (text && window.getSelection().toString()) {
-        ev.preventDefault();
+        ev.preventDefault()
         const e = [
           '作者：Nomad',
           "来自：Tom's Blog",
           '链接：' + window.location.href,
           '转载请注明出处~~',
           '',
-          window.getSelection().toString()
-        ];
-        text.setData('text/html', e.join('<br>'));
-        text.setData('text/plain', e.join('\n'));
+          window.getSelection().toString(),
+        ]
+        text.setData('text/html', e.join('<br>'))
+        text.setData('text/plain', e.join('\n'))
       }
     },
-    false
-  );
-})();
+    false,
+  )
+})()
 
 /**
  * inject some useful methods on window
  */
-(function() {
-  const observerAcive = 'observer-actived';
+;(function() {
+  const observerAcive = 'observer-actived'
 
   window.DOMObserver = function($target, cb) {
     new IntersectionObserver(
@@ -91,15 +91,15 @@ window.addEventListener('DOMContentLoaded', _ => {
             entry.intersectionRatio > 0 &&
             !$target.classList.contains(observerAcive)
           ) {
-            cb && cb.call($target, $target);
-            $target.classList.add(observerAcive);
+            cb && cb.call($target, $target)
+            $target.classList.add(observerAcive)
           }
-        });
+        })
       },
       {
         rootMargin: '0px',
-        threshold: 1.0
-      }
-    ).observe($target);
-  };
-})();
+        threshold: 1.0,
+      },
+    ).observe($target)
+  }
+})()
